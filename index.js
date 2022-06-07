b1f23685ed84aa0284670d49f1a38583a8eec057
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-07 13:25:32 -0400

    commit tree 2fd88377d2fe98ae7c364693ac8120491906446a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3 = 14
+const num4 = 5
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
